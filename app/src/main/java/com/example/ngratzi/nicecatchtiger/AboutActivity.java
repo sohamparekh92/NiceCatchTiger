c1d50292f1def6268fc4792a1a875d57a1662150
Soham <sohamap@g.clemson.edu>
2017-01-07 15:22:36 -0500

    About Activity Complete

--- app/src/main/java/com/example/ngratzi/nicecatchtiger/AboutActivity.java
+++ app/src/main/java/com/example/ngratzi/nicecatchtiger/AboutActivity.java
@@ -6,6 +6,7 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Html;
+import android.text.method.LinkMovementMethod;
 import android.widget.TextView;
 
 public class AboutActivity extends AppCompatActivity {
@@ -33,39 +34,50 @@ public class AboutActivity extends AppCompatActivity {
 
         String intro = "<html><font color=\"#000000\"" +
 
-                "<p>" +  "<i><b>Nice Catch Tiger!</b></i><br>" +
-                "</p><br>" +
-                "</font></html>";
-        String introContentstring = "This App shares close calls and near misses in order to alert others and improve safety" +
+                "<p>" +  "<i><b>Nice Catch Tiger!</b></i><br><br>" +
+                "This App shares close calls and near misses in order to alert others and improve safety " +
                 "awareness. If you see or experience a close call that if not \"caught\" in time could have resulted" +
-                " in a serious injury, Please user this app to contact" +
-                "Research Safety.";
+                " in a serious injury, please use this app to contact Research Safety."+
+                "</p>" +
+                "</font></html>";
         String definitionString = "<html><font color=\"#000000\"" +
 
-                "<p align="+ "\"" +"left" + "\""+ ">" +  "<b>About:</b>MY data" +"</p>"+
-
+                "<p>" +  "<b>Definition of a Near Miss</b><br><br>" +
+                "Incidents or events that could have resulted in injuries or other adverse consequences but fortunately did not."+
+                "</p><br>" +
                 "</font></html>";
         String submittingString = "<html><font color=\"#000000\"" +
 
-                "<p align="+ "\"" +"left" + "\""+ ">" +  "<b>About:</b>MY data" +"</p>"+
-
+                "<p>" +  "<b>Once submitted, Research Safety will:</b><br><br></p>" +
+                "<ul>" +
+                "<li>  • Review and follow up submittals </li><br><br>" +
+                "<li>  • Share information and \"lesson learned\" with ressearchers across campus (without identifying specific" +
+                "lab or staff when requested) </li><br><br>" +
+                "<li>  • Appreciate your submittal and commitment to safety </li><br><br>" +
+                "</ul>"+
                 "</font></html>";
+
         String italicString = "<html><font color=\"#000000\"" +
 
-                "<p align="+ "\"" +"left" + "\""+ "><i>" +  "<b>About:</b>MY data" +"</i></p>"+
-
+                "<p>" +
+                "<i>"+
+                "Please share your experiences with us so that we may continue to improve the safety culture at Clemson University."+
+                "</i>"+
+                "</p><br>" +
                 "</font></html>";
         String hotlineString = "<html><font color=\"#000000\"" +
 
-                "<p align="+ "\"" +"left" + "\""+ ">" +  "<b>About:</b>MY data" +"</p>"+
+                "<p align="+ "\"" +"left" + "\""+ ">" +  "If you have a serious safety" +
+                " or ethical concern but do not feel comfortable sharing your identity," +
+                " please use the confidential <a href=\"http://www.clemson.edu/administration/internalaudit/ethicsline.html\">hotline</a>.</p>"+
 
                 "</font></html>";
 
         introText.setText(Html.fromHtml(intro));
-        introContent.setText(introContentstring);
         definition.setText(Html.fromHtml(definitionString));
         submitting.setText(Html.fromHtml(submittingString));
         italic.setText(Html.fromHtml(italicString));
         hotline.setText(Html.fromHtml(hotlineString));
+        hotline.setMovementMethod(LinkMovementMethod.getInstance());
     }
 }
